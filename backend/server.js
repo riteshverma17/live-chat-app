b4008a736f3001a2e2a789267f05e76c26980c95
riteshverma17 <riteshjii.04@gmail.com>
2025-02-24 13:14:47 +0530

    new updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,8 @@ import {app, server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -22,7 +25,11 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 
 
