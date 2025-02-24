832a9133339e32182b32e2ea319121e05202f07c
riteshverma17 <riteshjii.04@gmail.com>
2025-02-24 17:41:42 +0530

    new updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-import SignUp from "./pages/SignUp/SignUp.jsx";
+import SignUp from "./pages/signUp/SignUp.jsx";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
