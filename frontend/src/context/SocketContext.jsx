7b2fd4470bcb4ed339425433bc90273f29bb8930
riteshverma17 <riteshjii.04@gmail.com>
2025-02-24 17:47:55 +0530

    new updates

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatify-oi6f.onrender.com", {
                 query:{
                     userId: authUser._id,
                 },
